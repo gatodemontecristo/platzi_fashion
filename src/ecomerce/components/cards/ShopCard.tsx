943e1b16fe3efd8984c2a43b45d6f3fbf8ea6eba
gatodemontecristo <erickdevicente2024@outlook.com>
2024-09-19 17:37:36 -0500

    feat: added prettier, eslint and husky configuration

--- src/ecomerce/components/cards/ShopCard.tsx
+++ src/ecomerce/components/cards/ShopCard.tsx
@@ -1,15 +1,10 @@
-import { CardProps } from '../../types';
-import {
-    BookmarkIcon,
-    PlusIcon
-  } from "@heroicons/react/24/outline";
-
-
+import { CardProps } from "../../types";
+import { BookmarkIcon, PlusIcon } from "@heroicons/react/24/outline";
 interface ShopCardProps {
-    card: CardProps;
+  card: CardProps;
 }
 
-export const ShopCard = ({card}:ShopCardProps) => {
+export const ShopCard = ({ card }: ShopCardProps) => {
   return (
     <div className="flex flex-col w-[20%]">
       <div className="relative">
@@ -22,7 +17,7 @@ export const ShopCard = ({card}:ShopCardProps) => {
           alt=""
         />
         <button className="absolute bottom-8 left-1/2 rounded-full bg-white bg-opacity-40 p-1">
-            <PlusIcon className="w-4 h-4 text-gray-600" />
+          <PlusIcon className="w-4 h-4 text-gray-600" />
         </button>
       </div>
       <div className="flex flex-col">
--- src/ecomerce/components/promo/BlackWhiteCover.tsx
+++ src/ecomerce/components/promo/BlackWhiteCover.tsx
@@ -1,13 +1,11 @@
-import React from 'react'
-
 export const BlackWhiteCover = () => {
   return (
     <div className="w-full">
-    <img
-      className="w-full filter grayscale"
-      src="../gallery/secondary01.jpg"
-      alt=""
-    />
-  </div>
-  )
-}
+      <img
+        className="w-full filter grayscale"
+        src="../gallery/secondary01.jpg"
+        alt=""
+      />
+    </div>
+  );
+};

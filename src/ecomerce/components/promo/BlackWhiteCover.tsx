95e557db2648146c4a1c0439c1136883ba88506c
gatodemontecristo <erickdevicente2024@outlook.com>
2024-10-24 15:42:16 -0500

    feat: added cellphone responsive

--- src/ecomerce/components/promo/BlackWhiteCover.tsx
+++ src/ecomerce/components/promo/BlackWhiteCover.tsx
@@ -6,7 +6,7 @@ export const BlackWhiteCover = () => {
         src="../gallery/secondary01.jpg"
         alt=""
       />
-      <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center text-white opacity-20 leading-none font-bold text-[170px] overflow-hidden">
+      <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center text-white opacity-20 leading-none font-bold lg:text-[170px] md:text-[100px] text-[40px] overflow-hidden">
         <div className="grid grid-cols-3 gap-4 text-center">
           {Array.from({ length: 15 }).map((_, index) => (
             <p key={index}>PLATZI</p>
